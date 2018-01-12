81740590003ea2f4ee154e23cde31c7b347391e7
Jeffrey Cave <jcavejr@gmail.com>
2018-01-12 01:18:26 +0000

    some changes for clarity

--- robinsons_stuff/fixed_angle/AngleRoutine.py
+++ robinsons_stuff/fixed_angle/AngleRoutine.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../../imports')
 from InitProb import *
+import math
 
 def create_quad_list():
     angle0 = 179
@@ -34,6 +35,17 @@ def create_quad_list_v2(lowerbound, pitch=5):
         angle3 = lowerbound
     return quad_list
 
+def get_distances(quad):
+    """Takes a quad and finds the smallest distance between 2 adjacent points and largest 
+    distance between 2 adjacent points. quad should be of the form: [[x0,y0],[x1,y1],[x2,y2],[x3,y3]]"""
+    distances = []
+    for i in range(4):
+        if i < 3:
+            distances.append(math.sqrt( ((quad[i][0]-quad[i+1][0])**2)+((quad[i][1]-quad[i+1][1])**2)  ))
+        else:
+            distances.append(math.sqrt( ((quad[3][0]-quad[0][0])**2)+((quad[3][1]-quad[0][1])**2)  ))
+    return distances
+
 if __name__ == '__main__':
     """
     Haven't gotten scipy's optimization to work yet.
@@ -43,7 +55,7 @@ if __name__ == '__main__':
     #templates = create_quad_list_v2(50)
     #templates = [[30, 60, 150, 120]]
 #    templates = [[30,150,120,60], [30, 69, 116, 145], [30, 120, 150, 60], [30, 116, 145, 69], [30, 150, 60, 120], [30, 116, 69, 145]]  #Angles
-    templates = [[135, 45, 45, 135], [135, 45, 135, 45]]
+    templates = [[135, 45, 45, 135]]
     #templates = [ [75, 110, ] , [] ]
     #for i, item in enumerate(templates):
     #    templates[i] = SPAlt(item)
@@ -74,6 +86,7 @@ if __name__ == '__main__':
         RARs = []
         xlist = []
 
+        # Generate list of quads
         for i in range(1, n):
             x = minx + i*pitch
             xlist.append(x)
@@ -82,18 +95,27 @@ if __name__ == '__main__':
         minRAR = float("inf")
         minQuad = None
         minOffset = float("inf")
+        # Generate list of RARs
         for quad in quads:
             RARs.append(robinsonAspectRatio(quad))
             if RARs[-1] < minRAR:
                 minRAR = RARs[-1]
                 minQuad = quad
                 minOffset = quad[1][0]
+            #Print some stuff about distance between adjacent points
+            if quad[1][0] % 1 == 0:
+                distances = get_distances(quad)
+                ratio = min(distances)/max(distances)
+                avg = sum(distances)/len(distances)
+                print("For AR: {}, {:4f}, {:4f}".format(int(quad[1][0]), ratio, avg))
 
         name = str(template).strip("[]").replace(",","_").replace(" ","")
 
         #print(xlist)
         #print(RARs)
         plt.plot(xlist, RARs)
+        plt.xlabel("Offset")
+        plt.ylabel("Robinson's Aspect Ratio")
         plt.axis([minx,maxx,minx, 5])
         plt.title(str(template))
         plt.savefig(name)
