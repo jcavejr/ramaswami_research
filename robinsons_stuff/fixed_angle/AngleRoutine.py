3b497e86115bdd6a07d06eccfaa855f6ecb158d2
Jeff Cave <jcavejr@gmail.com>
2018-01-25 09:57:00 -0500

    most recent

--- robinsons_stuff/fixed_angle/AngleRoutine.py
+++ robinsons_stuff/fixed_angle/AngleRoutine.py
@@ -46,19 +46,57 @@ def get_distances(quad):
             distances.append(math.sqrt( ((quad[3][0]-quad[0][0])**2)+((quad[3][1]-quad[0][1])**2)  ))
     return distances
 
+def get_max_distance(quad):
+    """Takes a quad and finds the radius for a circle that fits the quad.
+    quad should be of the form: [[x0,y0],[x1,y1],[x2,y2],[x3,y3]]"""
+    distances = []
+    distances.append(math.sqrt( ((quad[0][0]-quad[2][0])**2)+((quad[0][1]-quad[2][1])**2) ))
+    distances.append(math.sqrt( ((quad[1][0]-quad[3][0])**2)+((quad[1][1]-quad[3][1])**2) ))
+    return max(distances)
+
+def get_circle_area(quad):
+    """Returns area of a circle. Radius can be either a float or an int."""
+    radius = get_max_distance(quad)/2
+    return math.pi*(radius**2)
+
+def get_square_area(quad):
+    """Returns area of a square that has a diagonal the length of the max distance between 2 points from the given quad."""
+    diagonal = get_max_distance(quad)
+    #Using pythagorean theorem to find the length of the square's edges
+    length = math.sqrt((diagonal**2)/2)
+    square = [ [0,0],[length, 0],[length, length],[0, length]]
+    return get_quad_area(square)
+    
+    
+
+def get_quad_area(quad):
+    """quad is [[x0,y0],[x1,y1],[x2,y2],[x3,y3]]"""
+    abx = quad[1][0]-quad[0][0]
+    aby = quad[1][1]-quad[0][1]
+    acx = quad[2][0]-quad[0][0]
+    acy = quad[2][1]-quad[0][1]
+    tri1 = (abx*acx)+(aby*acy)
+    tri1 = 1/2*math.sqrt((abx**2+aby**2)*(acx**2+acy**2)-(tri1**2))
+
+    adx = quad[3][0]-quad[0][0]
+    ady = quad[3][1]-quad[0][1]
+    tri2 = (acx*adx)+(acy*ady)
+    tri2 = 1/2*math.sqrt((acx**2+acy**2)*(adx**2+ady**2)-(tri2**2))
+    return tri1 + tri2
+
 if __name__ == '__main__':
     """
     Haven't gotten scipy's optimization to work yet.
     This just creates a test.off file containing some sample quads
     """
-
+    #templates = [[90,90,90,90]]
     #templates = create_quad_list_v2(50)
     #templates = [[30, 60, 150, 120]]
 #    templates = [[30,150,120,60], [30, 69, 116, 145], [30, 120, 150, 60], [30, 116, 145, 69], [30, 150, 60, 120], [30, 116, 69, 145]]  #Angles
-    templates = [[135, 45, 45, 135]]
+    templates = [[135, 45, 45, 135], [90,90,90,90]]
     #templates = [ [75, 110, ] , [] ]
     #for i, item in enumerate(templates):
-    #    templates[i] = SPAlt(item)
+     #   templates[i] = SPAlt(item)
 
 #        templates.append(SPAlt(randomConvexQuad(40, 140)))
 
@@ -77,7 +115,7 @@ if __name__ == '__main__':
     """
     for i in range(len(templates)):
         minx = 0
-        maxx = 10
+        maxx = 5
         pitch = 1/20
         n = int((maxx-minx)/pitch)
 
@@ -95,6 +133,8 @@ if __name__ == '__main__':
         minRAR = float("inf")
         minQuad = None
         minOffset = float("inf")
+        circle_ratios = []
+        square_ratios = []
         # Generate list of RARs
         for quad in quads:
             RARs.append(robinsonAspectRatio(quad))
@@ -103,26 +143,41 @@ if __name__ == '__main__':
                 minQuad = quad
                 minOffset = quad[1][0]
             #Print some stuff about distance between adjacent points
-            if quad[1][0] % 1 == 0:
-                distances = get_distances(quad)
-                ratio = min(distances)/max(distances)
-                avg = sum(distances)/len(distances)
-                print("For AR: {}, {:4f}, {:4f}".format(int(quad[1][0]), ratio, avg))
+            #ratios.append(get_quad_area(quad)/get_circle_area(quad)) 
+            circle_ratios.append(get_quad_area(quad)/get_circle_area(quad))
+            square_ratios.append(get_quad_area(quad)/get_square_area(quad))
+            #distances = get_distances(quad)
+            #if quad[1][0] % 1 == 0:
+                #print(distances)    
+            #    print("For AR >>{:4f}<< circle={:4f}, quad={:4f}, ratio={:4f}".format(RARs[-1], get_circle_area(get_radius(quad)), get_quad_area(quad), get_circle_area(get_radius(quad))/ get_quad_area(quad)))
 
         name = str(template).strip("[]").replace(",","_").replace(" ","")
 
         #print(xlist)
         #print(RARs)
-        plt.plot(xlist, RARs)
+        plt.plot(xlist, RARs, label="Robinson's")
+        plt.plot(xlist, circle_ratios, label="Circle")
+        plt.plot(xlist, square_ratios, label="Square")
+        plt.legend(loc="upper right")
         plt.xlabel("Offset")
-        plt.ylabel("Robinson's Aspect Ratio")
+        plt.ylabel("Aspect Ratio")
         plt.axis([minx,maxx,minx, 5])
         plt.title(str(template))
         plt.savefig(name)
         plt.clf()
-        quads = [unitQuad(template), minQuad]
+        off_quads = [unitQuad(template), minQuad]
 
         offname = name + ".off"
-        exportToOFF( quads, offname )
+        exportToOFF( off_quads, offname )
+        #setting a constant to find best part of a curve
+        cutoff = .03
+        for i in range(1, len(RARs)):
+            if RARs[i-1]-RARs[i] < 0:
+                break
+            elif RARs[i-1]-RARs[i] < cutoff:
+                print("Cutoff was reached at Robinson's: {:4f}".format(RARs[i]))
+                print("Offset: {:2f}".format(i*pitch))
+                print("Edge Lengths:", get_distances(quads[i]))
+                break
         print("Minimum Robinson's Aspect Ratio: {}".format(minRAR))
         print("Minimum Offset: {}".format(minOffset))
